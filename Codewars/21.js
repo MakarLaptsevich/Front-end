1234b9adc8171604288555ec27d2ccab19d97b54
MakarLaptsevich <makarabool@gmail.com>
2023-10-09 15:27:43 +0300

    Codewars

---
+++ Codewars/21.js
@@ -0,0 +1,24 @@
+There are pillars near the road. The distance between the pillars 
+is the same and the width of the pillars is the same. 
+Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar
+ in centimeters (without the width of the first and last pillar).
+
+
+
+
+ function pillars(numPill, dist, width) {
+  let sumDist = 0;
+  if (numPill == 1) {
+    sumDist = 0;
+  } else if (numPill == 2) {
+    sumDist = dist * 100;
+  } else if (numPill > 2) {
+    sumDist = dist * 100 * (numPill - 1) + (numPill - 2) * width;
+  }
+  return sumDist;
+}
---
+++ Codewars/999.js
@@ -0,0 +1,9 @@
+function arrayPlusArray(arr1, arr2) {
+    let arr = arr1.concat(arr2);
+    let sum = 0;
+    for (i = 0; i < arr.length; i++) {
+      sum += arr.length[i];
+    }
+    return sum;
+  }
+  arrayPlusArray([1, 2], [1, 2]);
